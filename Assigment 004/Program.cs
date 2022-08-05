d9a7237066bb2cc1e81abbe2f1cfbdd0d77d10ad
Support <Support@DESKTOP-SE5ODLJ>
2022-08-05 03:28:25 +0100

    Add project files.

---
+++ Assigment 004/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Assigment_004
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 23, 33, 34, 78, 90, 89, 23, 88, 12, 100, 89, 99, 46, 20, 31 };
+
+            int i, j, temp;
+                for (i = 0; i < array.Length; i++)
+                {
+                    for (j = i + 1; j < array.Length; j++)
+                    {
+                        if (array[i] < array[j])
+                        {
+                            temp = array[i];
+                            array[i] = array[j];
+                            array[j] = temp;
+                        }
+                    }
+                }
+            Console.Write("\nDescending order:\n");
+            for (i = 0; i < array.Length; i++)
+            {
+                Console.Write("{0} ", array[i]);
+            }
+            
+        }
+        }
+    }
